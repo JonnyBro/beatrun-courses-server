5a27070437ad36bd44327ff1badc5958da6fd237
Jonny_Bro (Nikita) <niktom.nikto@ya.ru>
2024-12-06 18:08:27 +0500

    undockerize

---
+++ config.example.js
@@ -0,0 +1,18 @@
+module.exports = {
+	/* Set true for production database */
+	production: true,
+	/* Your domain without / at the end */
+	domain: "http://localhost:6547",
+	/* Port for the server */
+	port: 6547,
+	/* How often can user send request to API */
+	rateLimitTime: 1000 * 5, // 5 seconds
+	/* How often can user change IP address */
+	ipChangeTime: 1000 * 60 * 60 * 3, // 3 hours
+	/* Your SteamAPI key */
+	steamKey: "",
+	/* Secret for a cookie */
+	cookieSecret: "",
+	/* Discord webhook url or leave empty */
+	webhook_url: "",
+};
