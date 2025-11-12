8a96af2d3ca1fe5c041dd58e2602dc037ca29e31
Jonny <jonnyar226@gmail.com>
2025-11-12 13:32:09 +0500

    refactor(frontend): api

--- packages/frontend/src/api/courses/index.ts
+++ packages/frontend/src/api/courses/index.ts
@@ -2,17 +2,17 @@ import { api, type Response } from "@/plugins/axios";
 import type { Course } from "./types";
 
 export const getCourses = async () => {
-	const response = await api.get<Response<Course[]>>("/courses/list");
+	const response = await api.get<Response<Course[]>>("/api/courses/list");
 	return response.data;
 };
 
 export const getCourseByCode = async (code: string) => {
-	const response = await api.get<Response<Course>>(`/courses/info/${code}`);
+	const response = await api.get<Response<Course>>(`/api/courses/info/${code}`);
 	return response.data;
 };
 
 export const downloadCourseByCode = async (code: string) => {
-	const response = await api.get<Response<string>>(`/courses/download`, {
+	const response = await api.get<Response<string>>(`/api/courses/download`, {
 		headers: {
 			code,
 		},
@@ -21,11 +21,11 @@ export const downloadCourseByCode = async (code: string) => {
 };
 
 export const likeCourse = async (code: string) => {
-	const response = await api.post(`/courses/like/${code}`);
+	const response = await api.post(`/api/courses/like/${code}`);
 	return response.data;
 };
 
 export const dislikeCourse = async (code: string) => {
-	const response = await api.post(`/courses/dislike/${code}`);
+	const response = await api.post(`/api/courses/dislike/${code}`);
 	return response.data;
 };
