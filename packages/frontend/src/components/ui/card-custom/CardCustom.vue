c3edb63a5ea7a7764099f037107af98763b5d32e
Jonny <jonnyar226@gmail.com>
2026-02-05 11:25:52 +0500

    readd components (?)

---
+++ packages/frontend/src/components/ui/card-custom/CardCustom.vue
@@ -0,0 +1,14 @@
+<template>
+	<div
+		class="flex items-stretch flex-row w-fit shadow-xl rounded-xl bg-card-foreground/70 dark:bg-accent-foreground/75"
+	>
+		<div class="relative">
+			<slot name="image" />
+		</div>
+		<div class="p-2 font-mono text-accent min-w-0">
+			<slot />
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts"></script>
