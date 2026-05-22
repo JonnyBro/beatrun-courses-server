41bc78143009b812a2055c316a9e1877ecbd8189
JonnyBro <jonnyar226@gmail.com>
2026-05-22 10:23:12 +0500

    update index page

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -2,7 +2,10 @@ import { FastifyInstance } from "fastify";
 
 const router = (fastify: FastifyInstance, _options: object) => {
 	fastify.get("/", (_req, reply) => {
-		reply.status(200).send({ code: reply.statusCode, message: "Hello World!" });
+		reply.status(200).send({
+			code: reply.statusCode,
+			message: "Hello World! You can access the database from the game (press F4)",
+		});
 	});
 };
 
